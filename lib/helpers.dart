b3d2004cea79244da94f3575689e4093f922c638
Habeeb <ahmed.h@amk.sa>
2025-02-03 09:36:03 +0200

    sudi arabia

--- lib/helpers.dart
+++ lib/helpers.dart
@@ -18,14 +18,14 @@ String removeDiacritics(String str) {
 
 bool validate(String value, String countryCode) {
   Set egypt = {'10', '11', '12', '15'};
-  Set saudiArabia = {'17', '13', '11', '16', '12', '14'};
+  Set saudiArabia = {'5', };
   //validate to egypt and saudi arabia to check in egypt must start with 10 ,11 ,12 and 15
   switch (countryCode) {
     case 'EG':
       return (egypt.contains(value.substring(0, 2)));
 
     case 'SA':
-      return (saudiArabia.contains(value.substring(0, 2)));
+      return (saudiArabia.contains(value.substring(0, 1)));
 
     default:
       return false;
